5084e4768a32435607b1574e77399be92af552b3
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2020-09-11 12:21:33 +0200

    Added some API calls in a new ViewModel for hotel search

---
+++ HolidayMakerUWP/HolidayMakerUWP/ViewModel/HotelSearchViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using HolidayMakerUWP.Model;
+
+namespace HolidayMakerUWP.ViewModel
+{
+    class HotelSearchViewModel
+    {
+        public ObservableCollection<Room> Rooms { get; set; }
+
+        private HttpClient httpClient;
+        private string roomURL = "";
+
+        public HotelSearchViewModel()
+        {
+            Rooms = new ObservableCollection<Room>();
+            httpClient = new HttpClient();
+        }
+
+        public async Task<ObservableCollection<Room>> GetAllRoomsAsync()
+        {
+            var jsonRooms = await httpClient.GetStringAsync(roomURL);
+
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.MissingMemberHandling = MissingMemberHandling.Error;
+
+            var rooms = JsonConvert.DeserializeObject < ObservableCollection<Room>(jsonRooms, settings);
+
+            return rooms;
+        }
+    }
+}
