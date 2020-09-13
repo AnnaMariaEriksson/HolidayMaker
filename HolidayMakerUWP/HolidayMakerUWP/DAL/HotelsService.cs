b56439df33c208447a0cbf6616b1982cdcf21546
Ande <andre.roygaard@gmail.com>
2020-09-13 03:02:33 +0200

    Har fixat alla filtrena bara slidsen kvar.

---
+++ HolidayMakerUWP/HolidayMakerUWP/DAL/HotelsService.cs
@@ -0,0 +1,26 @@
+﻿using HolidayMakerUWP.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayMakerUWP.DAL
+{
+   public class HotelsService
+    {
+        public ObservableCollection<Hotel> GetHotels()
+        {
+            ObservableCollection<Hotel> Hotels = new ObservableCollection<Hotel>();
+            Hotel h1 = new Hotel() { Name = "Hotel 1", FilterReset = true, DistansToCenter = 40, DistansToBeach = 10, HasAllInclusive = false, HasFullBoard = true, HasHalfBoard = false, HasRestaurant = true, HasChildrensClub = false, HasEntertainment = true, HasPool = false, HotelID = 1 };
+            Hotel h2 = new Hotel() { Name = "Hotel 2", FilterReset = true, DistansToCenter = 30, DistansToBeach = 25, HasAllInclusive = true, HasFullBoard = false, HasHalfBoard = true, HasRestaurant = true, HasChildrensClub = false, HasEntertainment = true, HasPool = false, HotelID = 2 };
+            Hotel h3 = new Hotel() { Name = "Hotel 3", FilterReset = true, DistansToCenter = 50, DistansToBeach = 45, HasAllInclusive = false, HasFullBoard = false, HasHalfBoard = false, HasRestaurant = true, HasChildrensClub = false, HasEntertainment = true, HasPool = false, HotelID = 3 };
+            Hotels.Add(h1);
+            Hotels.Add(h2);
+            Hotels.Add(h3);
+
+            return Hotels;
+        }
+    }
+}
--- HolidayMakerUWP/HolidayMakerUWP/Model/Hotel.cs
+++ HolidayMakerUWP/HolidayMakerUWP/Model/Hotel.cs
@@ -9,15 +9,17 @@ namespace HolidayMakerUWP.Model
    public class Hotel
     {
         public  int HotelID { get; set; }
+        public string Name { get; set; }
         public  int DistansToBeach { get; set; }
         public  int DistansToCenter { get; set; }
-        public string HasAllInclusive { get; set; }
-        public  string HasFullBoard { get; set; }
-        public  string HasHalfBoard { get; set; }
+        public bool HasAllInclusive { get; set; }
+        public  bool HasFullBoard { get; set; }
+        public  bool HasHalfBoard { get; set; }
         public  bool HasRestaurant { get; set; }
         public  bool HasChildrensClub { get; set; }
         public  bool HasEntertainment { get; set; }
         public  bool HasPool { get; set; }
+        public bool FilterReset { get; set; }
 
         public Hotel()
         {
