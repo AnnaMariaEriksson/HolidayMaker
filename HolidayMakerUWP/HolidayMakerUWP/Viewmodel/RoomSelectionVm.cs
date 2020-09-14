accf993cd83f4b8a865f9331e4955e92db815f23
Saimir avdija <katundari_@hotmail.com>
2020-09-14 14:54:12 +0200

    höll på med RoomSelection-sidan

---
+++ HolidayMakerUWP/HolidayMakerUWP/Viewmodel/RoomSelectionVm.cs
@@ -0,0 +1,34 @@
+﻿using HolidayMakerUWP.DAL;
+using HolidayMakerUWP.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayMakerUWP.Viewmodel
+{
+    public class RoomSelectionVm
+    {
+
+        public HotelsService Vm;
+        public ObservableCollection<Room> _rooms { get; set; }
+        public ObservableCollection<Room> Rooms
+        {
+            get
+            {
+                return _rooms;
+            }
+            set
+            {
+                _rooms = value;
+            }
+        }
+        public RoomSelectionVm()
+        {
+            this.Vm = new HotelsService();
+            _rooms = Vm.GetRooms();
+        }
+    }
+}
