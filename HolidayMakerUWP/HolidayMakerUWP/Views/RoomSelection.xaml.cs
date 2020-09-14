f8955a04aff240cc0b075236f54b77f4e90ef8af
Saimir avdija <katundari_@hotmail.com>
2020-09-14 23:32:24 +0200

    able to take in description on hotel, added listview holding the hotels.

--- HolidayMakerUWP/HolidayMakerUWP/Views/RoomSelection.xaml.cs
+++ HolidayMakerUWP/HolidayMakerUWP/Views/RoomSelection.xaml.cs
@@ -1,9 +1,13 @@
-﻿using HolidayMakerUWP.Viewmodel;
+﻿using HolidayMakerUWP.Model;
+using HolidayMakerUWP.Viewmodel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -24,10 +28,13 @@ namespace HolidayMakerUWP.Views
     public sealed partial class RoomSelection : Page
     {
         public RoomSelectionVm Vm;
+        public ObservableCollection<Hotel> tempHotels = new ObservableCollection<Hotel>();
         public RoomSelection()
         {
             this.InitializeComponent();
             this.Vm = new RoomSelectionVm();
+            Hotel r1 = new Hotel() { HotelDescription = "Very nice hotel", Name = "Hotel 1", FilterReset = true, DistansToCenter = 40, DistansToBeach = 10, HasAllInclusive = false, HasFullBoard = true, HasHalfBoard = false, HasRestaurant = true, HasChildrensClub = false, HasEntertainment = true, HasPool = false, HotelID = 1 };
+            tempHotels.Add(r1);
         }
     }
 }
