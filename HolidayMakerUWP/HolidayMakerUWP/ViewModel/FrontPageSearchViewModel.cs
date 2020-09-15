227a3aef90cabbad5f6f4403a58ba6a5121dfd31
Ande <andre.roygaard@gmail.com>
2020-09-15 12:55:47 +0200

    fixade merge med frontpage

---
+++ HolidayMakerUWP/HolidayMakerUWP/ViewModel/FrontPageSearchViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HolidayMakerUWP.Model;
+using Newtonsoft.Json;
+
+namespace HolidayMakerUWP.Viewmodel
+{
+    public class FrontPageSearchViewModel
+    {
+        public ObservableCollection<Room> Rooms { get; set; }
+        public ObservableCollection<Regions> Regions { get; set; }
+        public ObservableCollection<City> Cities { get; set; }
+        public HttpClient httpClient;
+
+        //Just a comment.
+
+        public FrontPageSearchViewModel()
+        {
+           
+            Regions = new ObservableCollection<Regions>();
+            Cities = new ObservableCollection<City>();
+            httpClient = new HttpClient();
+        }
+
+       
+
+    }
+}
