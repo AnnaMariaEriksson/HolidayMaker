425a4f8d844566e595259fd3df181eb1ebcb1cb1
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2020-09-15 12:17:20 +0200

    Moved async methods and API calls to the correct file.

--- HolidayMakerUWP/HolidayMakerUWP/DAL/HotelsService.cs
+++ HolidayMakerUWP/HolidayMakerUWP/DAL/HotelsService.cs
@@ -3,13 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace HolidayMakerUWP.DAL
 {
    public class HotelsService
     {
+
+        private HttpClient httpClient;
+
+        private string roomURL = "";
+
+        private string regionURL = "";
+
+        private string cityURL = "";
         public ObservableCollection<Hotel> GetHotels()
         {
             ObservableCollection<Hotel> Hotels = new ObservableCollection<Hotel>();
@@ -23,7 +33,77 @@ namespace HolidayMakerUWP.DAL
             return Hotels;
         }
 
-        
+        public async Task<ObservableCollection<Room>> GetAllRoomsAsync()
+        {
+            var jsonRooms = await httpClient.GetStringAsync(roomURL);
 
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.MissingMemberHandling = MissingMemberHandling.Error;
+
+            var rooms = JsonConvert.DeserializeObject<ObservableCollection<Room>>(jsonRooms, settings);
+
+            return rooms;
+        }
+
+        public async Task<ObservableCollection<Regions>> GetAllRegionsAsync()
+        {
+            var jsonRegions = await httpClient.GetStringAsync(regionURL);
+
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.MissingMemberHandling = MissingMemberHandling.Error;
+
+            var regions = JsonConvert.DeserializeObject<ObservableCollection<Regions>>(jsonRegions, settings);
+
+            return regions;
+        }
+
+        public async Task<ObservableCollection<Regions>> GetAllCitiesAsync()
+        {
+            var jsonCities = await httpClient.GetStringAsync(regionURL);
+
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.MissingMemberHandling = MissingMemberHandling.Error;
+
+            var cities = JsonConvert.DeserializeObject<ObservableCollection<Regions>>(jsonCities, settings);
+
+            return cities;
+        }
+
+
+        public ObservableCollection<Regions> GetRegions()
+        {
+            ObservableCollection<Regions> Regions = new ObservableCollection<Regions>();
+            // todo fix the list below so it's accessible.
+            // List<Hotel> hotels = GetHotels().ToList();
+            var cities = new List<City>
+            {
+                new City { NameOfCity = "Malmö", CityID = 1, RegionID = 1},
+                new City {NameOfCity = "Lund", CityID = 2, RegionID = 1},
+                new City {NameOfCity = "Kristianstad", CityID = 3, RegionID = 1}
+            };
+            Regions r1 = new Regions
+            {
+                NameOfRegion = "Skåne",
+                RegionID = 1,
+                Cities = cities
+            };
+
+            Regions.Add(r1);
+
+            return Regions;
+        }
+
+        public ObservableCollection<Room> GetRooms()
+        {
+            ObservableCollection<Room> Rooms = new ObservableCollection<Room>();
+
+            Room room1 = new Room { HotelID = 1, IsAllInclusive = false, NumberOfBeds = 3, RoomID = 1 };
+
+            Rooms.Add(room1);
+
+            return Rooms;
+        }
     }
+
+}
 }
--- HolidayMakerUWP/HolidayMakerUWP/ViewModel/FrontPageSearchViewModel.cs
+++ HolidayMakerUWP/HolidayMakerUWP/ViewModel/FrontPageSearchViewModel.cs
@@ -13,61 +13,19 @@ namespace HolidayMakerUWP.Viewmodel
     public class FrontPageSearchViewModel
     {
         public ObservableCollection<Room> Rooms { get; set; }
+        public ObservableCollection<Regions> Regions { get; set; }
+        public ObservableCollection<City> Cities { get; set; }
 
-        private HttpClient httpClient;
-        private string roomURL = "";
+        
         //Just a comment.
 
         public FrontPageSearchViewModel()
         {
             Rooms = new ObservableCollection<Room>();
+            Regions = new ObservableCollection<Regions>();
+            Cities = new ObservableCollection<City>();
             httpClient = new HttpClient();
         }
 
-        public async Task<ObservableCollection<Room>> GetAllRoomsAsync()
-        {
-            var jsonRooms = await httpClient.GetStringAsync(roomURL);
-
-            JsonSerializerSettings settings = new JsonSerializerSettings();
-            settings.MissingMemberHandling = MissingMemberHandling.Error;
-
-            var rooms = JsonConvert.DeserializeObject<ObservableCollection<Room>>(jsonRooms, settings);
-
-            return rooms;
-        }
-
-        public ObservableCollection<Regions> GetRegions()
-        {
-            ObservableCollection<Regions> Regions = new ObservableCollection<Regions>();
-            // todo fix the list below so it's accessible.
-            // List<Hotel> hotels = GetHotels().ToList();
-            var cities = new List<City>
-            {
-                new City { NameOfCity = "Malmö", CityID = 1, RegionID = 1},
-                new City {NameOfCity = "Lund", CityID = 2, RegionID = 1},
-                new City {NameOfCity = "Kristianstad", CityID = 3, RegionID = 1}
-            };
-            Regions r1 = new Regions
-            {
-                NameOfRegion = "Skåne",
-                RegionID = 1,
-                Cities = cities
-            };
-
-            Regions.Add(r1);
-
-            return Regions;
-        }
-
-        public ObservableCollection<Room> GetRooms()
-        {
-            ObservableCollection<Room> roomsObservableCollection = Rooms;
-
-            Room room1 = new Room { HotelID = 1, IsAllInclusive = false, NumberOfBeds = 3, RoomID = 1 };
-
-            Rooms.Add(room1);
-
-            return Rooms;
-        }
-    }
+        
 }
