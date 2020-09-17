8769dcf9501ba64e6cbec7c5876b19bbb29bfaed
Andre3671 <55064742+Andre3671@users.noreply.github.com>
2020-09-17 11:49:41 +0200

    Revert "Frontpage search"

--- HolidayMakerUWP/HolidayMakerUWP/ViewModel/FrontPageSearchViewModel.cs
+++ HolidayMakerUWP/HolidayMakerUWP/ViewModel/FrontPageSearchViewModel.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HolidayMakerUWP.DAL;
 using HolidayMakerUWP.Model;
+using Newtonsoft.Json;
 
 namespace HolidayMakerUWP.Viewmodel
 {
@@ -10,9 +16,8 @@ namespace HolidayMakerUWP.Viewmodel
         public ObservableCollection<Regions> Regions { get; set; }
         public ObservableCollection<City> Cities { get; set; }
         public ObservableCollection<City> TempCity { get; set; }
-        public Search Search { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+
+        public HotelsService HotelsService { get; set; }
 
         public FrontPageSearchViewModel()
         {
@@ -32,7 +37,7 @@ namespace HolidayMakerUWP.Viewmodel
             Cities.Add(new City{NameOfCity = "Nån stad i Skåne", RegionID = 1});
             Cities.Add(new City { NameOfCity = "En till stad i Skåne", RegionID = 1 });
             Cities.Add(new City{NameOfCity = "Nån stad i Sthlm", RegionID = 2, CityID = 4});
-            
+
         }
 
     }
