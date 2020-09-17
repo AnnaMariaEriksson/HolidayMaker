28488e47f1b005f1788cf5c686697765f04d3a1c
Ande <andre.roygaard@gmail.com>
2020-09-17 15:35:21 +0200

    Påbörjade databasen. och gjorde klart controllern för inloggning och antagligen rummet.osäker om det funkar förrän vi testat.

---
+++ HolidayMakerBackend/HolidayMakerBackend/Models/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HolidayMakerBackend.Models
+{
+    public class Room
+    {
+        public int RoomID { get; set; }
+        public int HotelID { get; set; }
+        public int Price { get; set; }
+        public bool ExtraBed { get; set; }
+        public bool HasAllInclusive { get; set; }
+        public bool IsAllInclusive { get; set; }
+        public bool HasFullBoard { get; set; }
+        public bool HasHalfBoard { get; set; }
+        public string RoomName { get; set; }
+        public int NumberOfBeds { get; set; }
+
+        public Room()
+        {
+
+        }
+    }
+}
--- HolidayMakerBackend/HolidayMakerBackend/Startup.cs
+++ HolidayMakerBackend/HolidayMakerBackend/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using HolidayMakerBackend.Data;
 
 namespace HolidayMakerBackend
 {
@@ -26,6 +28,9 @@ namespace HolidayMakerBackend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<HolidayMakerBackendContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("HolidayMakerBackendContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
