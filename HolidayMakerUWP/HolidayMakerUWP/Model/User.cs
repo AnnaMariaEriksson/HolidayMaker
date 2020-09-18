d7b4b2e5ff6ccde8cb7f27c19a0a55f7e70d7734
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2020-09-18 18:10:53 +0200

    Re-added stuff for registration page

--- HolidayMakerUWP/HolidayMakerUWP/Model/User.cs
+++ HolidayMakerUWP/HolidayMakerUWP/Model/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace HolidayMakerUWP.Model
 {
     class User
     {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ObservableCollection<Booking> UserBookings { get; set; }
+        
     }
 }
